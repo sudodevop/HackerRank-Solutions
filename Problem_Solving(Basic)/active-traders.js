2a59f6e25e5317a73a841076d844e2c36d6e3bbf
Aryan Anshuman <sudodevop@gmail.com>
2024-02-23 22:15:10 +0530

    Adding Files

---
+++ Problem_Solving(Basic)/active-traders.js
@@ -0,0 +1,14 @@
+function mostActive(customers) {
+  let d = {};
+  let outArr = [];
+
+  for (let i of customers) {
+    d[i] = (d[i] || 0) + 1;
+  }
+
+  return Object.keys(d)
+    .filter((v) => {
+      return d[v] >= 0.05 * customers.length;
+    })
+    .sort();
+}
